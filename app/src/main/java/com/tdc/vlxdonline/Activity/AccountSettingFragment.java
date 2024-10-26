b08755b516b77b6d777daa35a9c9d4479981763a
Nguyen Van Linh <linhvng5420@gmail.com>
2024-10-26 20:24:11 +0700

    Chuẩn bị: Xử lý logic chức năng tài khoản

--- app/src/main/java/com/tdc/vlxdonline/Activity/AccountSettingFragment.java
+++ app/src/main/java/com/tdc/vlxdonline/Activity/AccountSettingFragment.java
@@ -1,16 +1,18 @@
 package com.tdc.vlxdonline.Activity;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.tdc.vlxdonline.R;
+import com.tdc.vlxdonline.databinding.FragmentAccountSettingBinding;
 
 public class AccountSettingFragment extends Fragment {
+    FragmentAccountSettingBinding binding;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,9 @@ public class AccountSettingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentAccountSettingBinding.inflate(inflater, container, false);
         return inflater.inflate(R.layout.fragment_account_setting, container, false);
     }
+
+        
 }
