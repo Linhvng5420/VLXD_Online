ba6e9e88112f53bd0b2658e57b70d278982fd5ad
Nguyen Van Linh <linhvng5420@gmail.com>
2024-11-06 07:33:18 +0700

    Update: Finish 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,23 +37,20 @@ android {
 }
 
 dependencies {
-    implementation(libs.glide)
-    implementation(libs.firebase.database)
-    implementation(libs.firebase.storage)
-    annotationProcessor(libs.compiler)
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
+    // Load Picture
+    implementation(libs.glide)
+    implementation(libs.compiler)
+
     // Import the Firebase
-    implementation ("com.google.firebase:firebase-database")
-    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation ("com.google.firebase:firebase-storage:21.0.1")
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.storage)
+    implementation(libs.firebase.bom)
 }
