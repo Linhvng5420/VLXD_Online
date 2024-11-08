34d6ce39679c82133d964b541a6280c4d523cea0
Nguyen Van Linh <linhvng5420@gmail.com>
2024-11-08 07:36:51 +0700

    Chuan Bi: Thông Tin Chủ Cửa Hàng

--- app/src/main/java/com/tdc/vlxdonline/Activity/Owner_SettingDetailFragment.java
+++ app/src/main/java/com/tdc/vlxdonline/Activity/Owner_SettingDetailFragment.java
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment;
 
 import com.tdc.vlxdonline.databinding.FragmentOwnerHosoBinding;
 
-public class Owner_HoSoFragment extends Fragment {
+public class Owner_SettingDetailFragment extends Fragment {
     FragmentOwnerHosoBinding binding;
 
     @Override
