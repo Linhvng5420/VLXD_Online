365e9b98ec75060b0eafd2c8b88541f4f589e54d
VanThien <Phuthien847@gmail.com>
2024-11-11 09:14:06 +0700

    bug

--- app/src/main/java/com/tdc/vlxdonline/Activity/TaiKhoanNVFragment.java
+++ app/src/main/java/com/tdc/vlxdonline/Activity/TaiKhoanNVFragment.java
@@ -11,9 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.tdc.vlxdonline.R;
+import com.tdc.vlxdonline.databinding.FragmentTaiKhoanNVBinding;
 
 public class TaiKhoanNVFragment extends Fragment {
-
+    FragmentTaiKhoanNVBinding binding;
 
 
     @Override
@@ -25,18 +26,34 @@ public class TaiKhoanNVFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tai_khoan_n_v, container, false);
+        binding = FragmentTaiKhoanNVBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        setControl();
         // Goi Event o day
+        binding.lnHoSo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_owner, new TaiKhoanNVHoSoFragment()) // Thay thế fragment_owner hiện tại bằng fragment chi tiết
+                        .addToBackStack(null) // Cho phép quay lại màn hình trước khi nhấn nút Back
+                        .commit(); // Thực hiện chuyển đổi
+            }
+        });
+
+        binding.lnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_owner, new Owner_SettingAboutFragment()) // Thay thế fragment_owner hiện tại bằng fragment chi tiết
+                        .addToBackStack(null) // Cho phép quay lại màn hình trước khi nhấn nút Back
+                        .commit(); // Thực hiện chuyển đổi
+            }
+        });
 
     }
 
-    private void setControl() {
-
-    }
 }
