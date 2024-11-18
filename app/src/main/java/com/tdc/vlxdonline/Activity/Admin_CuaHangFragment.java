8286d4d14bc95b79744f10c9ee24e23f493c6d46
Nguyen Van Linh <linhvng5420@gmail.com>
2024-11-18 10:49:28 +0700

    Admin - CuaHang: Show List CH

--- app/src/main/java/com/tdc/vlxdonline/Activity/Admin_CuaHangFragment.java
+++ app/src/main/java/com/tdc/vlxdonline/Activity/Admin_CuaHangFragment.java
@@ -1,31 +1,109 @@
 package com.tdc.vlxdonline.Activity;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.tdc.vlxdonline.Adapter.CuaHangAdapter;
+import com.tdc.vlxdonline.Model.ThongTinChu;
+import com.tdc.vlxdonline.R;
 import com.tdc.vlxdonline.databinding.FragmentAdminCuahangBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Admin_CuaHangFragment extends Fragment {
     FragmentAdminCuahangBinding binding;
+    String LoginEmailID = LoginActivity.accountID;
+    String LoginEmail = LoginActivity.idUser;
+    List<ThongTinChu> listChuCuaHang;
+    CuaHangAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        listChuCuaHang = new ArrayList<>();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         binding = FragmentAdminCuahangBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
-        
 
+        // Adapter - Recycleview
+        binding.ownerRcvNhanVien.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new CuaHangAdapter(listChuCuaHang);
+        binding.ownerRcvNhanVien.setAdapter(adapter);
+
+        // Get Data Firebase listChuCuaHang
+        getDataCuaHang();
+        setOnClickItemRecycleView();
+        //
         return view;
+    }
+
+    private void setOnClickItemRecycleView() {
+        adapter.setOnItemClickListener(new CuaHangAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(ThongTinChu cuahang) {
+                Bundle bundleIDKhachHang = new Bundle();
+                bundleIDKhachHang.putSerializable("idCH", cuahang.getId());
+                Owner_KhachHangDetailFragment khachHangDetailFragment = new Owner_KhachHangDetailFragment();
+                khachHangDetailFragment.setArguments(bundleIDKhachHang);
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, khachHangDetailFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+
+    private void getDataCuaHang() {
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference("thongtinchu");
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listChuCuaHang.clear();
+                adapter.getCuaHangList().clear();
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    ThongTinChu thongTinChu = new ThongTinChu();
+                    thongTinChu = dataSnapshot.getValue(ThongTinChu.class);
+
+                    if (thongTinChu != null) {
+                        thongTinChu.setId(dataSnapshot.getKey());
+                        listChuCuaHang.add(thongTinChu);
+                    } else {
+                        binding.tvTitle.setText("Danh Sách Cửa Hàng Rỗng");
+                        Log.d("l.d", "getDataCuaHang: null");
+                    }
+                }
+                Log.d("l.d", "getDataCuaHang: " + listChuCuaHang.toString());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
--- app/src/main/java/com/tdc/vlxdonline/Adapter/CuaHangAdapter.java
+++ app/src/main/java/com/tdc/vlxdonline/Adapter/CuaHangAdapter.java
@@ -2,10 +2,16 @@ package com.tdc.vlxdonline.Adapter;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.tdc.vlxdonline.Model.ThongTinChu;
 import com.tdc.vlxdonline.databinding.ItemOwnerRecycleviewBinding;
 
@@ -13,6 +19,7 @@ import java.util.List;
 
 public class CuaHangAdapter extends RecyclerView.Adapter<CuaHangAdapter.CuaHangViewHolder> {
     List<ThongTinChu> cuaHangList;
+    private OnItemClickListener onItemClickListener;
 
     public CuaHangAdapter(List<ThongTinChu> cuaHangList) {
         this.cuaHangList = cuaHangList;
@@ -24,6 +31,16 @@ public class CuaHangAdapter extends RecyclerView.Adapter<CuaHangAdapter.CuaHangV
 
     public void setCuaHangList(List<ThongTinChu> cuaHangList) {
         this.cuaHangList = cuaHangList;
+    }
+
+    // Giao diện cho sự kiện nhấn vào item
+    public interface OnItemClickListener {
+        void onItemClick(ThongTinChu cuahang); // Phương thức được gọi khi một item được nhấn
+    }
+
+    // Phương thức thiết lập listener cho sự kiện nhấn vào item
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.onItemClickListener = listener;
     }
 
     //Viewholder
@@ -38,11 +55,18 @@ public class CuaHangAdapter extends RecyclerView.Adapter<CuaHangAdapter.CuaHangV
     public void onBindViewHolder(@NonNull CuaHangAdapter.CuaHangViewHolder holder, int position) {
         ThongTinChu cuaHang = cuaHangList.get(position);
         holder.binding(cuaHang);
+
+        // Xử lý sự kiện nhấn vào item
+        holder.itemView.setOnClickListener(v -> {
+            if (onItemClickListener != null) {
+                onItemClickListener.onItemClick(cuaHang); // Gọi phương thức onItemClick khi item được nhấn
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return cuaHangList.size();
     }
 
     public class CuaHangViewHolder extends RecyclerView.ViewHolder {
@@ -55,10 +79,51 @@ public class CuaHangAdapter extends RecyclerView.Adapter<CuaHangAdapter.CuaHangV
 
         public void binding(ThongTinChu cuaHang) {
             binding.tvID.setText(cuaHang.getId());
-            binding.tvTen.setText(cuaHang.getTen());
             binding.tvSDT.setText(cuaHang.getSdt());
+            String tenCuaHang = "N/A";
+
+            DatabaseReference dbTenCuaHang = FirebaseDatabase.getInstance().getReference("thongtinchu/" + cuaHang.getId() + "/cuahang");
+
+            dbTenCuaHang.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String tenCuaHang = snapshot.getValue(String.class);
+                    binding.tvTen.setText(tenCuaHang != null && !tenCuaHang.equals("N/A") ? tenCuaHang : cuaHang.getTen());
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+            // Lấy trạng thái tài khoản cửa hàng
+            DatabaseReference db = FirebaseDatabase.getInstance().getReference("account/" + cuaHang.getId());
+            db.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    Boolean lock = snapshot.child("trangthai/lock").getValue(Boolean.class);
+                    Boolean online = snapshot.child("trangthai/online").getValue(Boolean.class);
+                    String locktime = snapshot.child("trangthai/locktime").getValue(String.class);
+                    String locktype = snapshot.child("trangthai/locktype").getValue(String.class);
+
+                    if (lock == null) {
+                        Toast.makeText(binding.getRoot().getContext(), "Lỗi đăng nhập tài khoản", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    if (lock) {
+                        binding.tvPhu.setText(locktype.equals("vinhvien") ? "Khóa Vĩnh Viễn" : "Khóa: " + locktime);
+                    } else {
+                        binding.tvPhu.setText(online ? "Online" : "Offline");
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    binding.tvPhu.setText("N/A");
+                }
+            });
         }
     }
-
-
 }
